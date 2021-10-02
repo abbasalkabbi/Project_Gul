79df901a1152b96a63fce010e480f5db4cf91c67
bro abbas <abbasalkabbi2003@gmail.com>
2021-10-02 04:05:05 +0300

    add Exit button and about page

---
+++ Project_Gul/About.h
@@ -0,0 +1,101 @@
+#pragma once
+
+namespace ProjectGul {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for About
+	/// </summary>
+	public ref class About : public System::Windows::Forms::Form
+	{
+	public:
+		About(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~About()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Label^ label1;
+	protected:
+
+	private: System::Windows::Forms::Button^ button1;
+
+	protected:
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(12, 25);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(407, 13);
+			this->label1->TabIndex = 1;
+			this->label1->Text = L"Full Stack Developer ,(Html,Css,Javascript,scss,bootstrap,Vue js,Php ,mysqli,C++)"
+				L"";
+			// 
+			// button1
+			// 
+			this->button1->Cursor = System::Windows::Forms::Cursors::Hand;
+			this->button1->Location = System::Drawing::Point(358, 322);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(200, 39);
+			this->button1->TabIndex = 3;
+			this->button1->Text = L"OK";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &About::button1_Click);
+			// 
+			// About
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(913, 411);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->label1);
+			this->Name = L"About";
+			this->Text = L"About";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		Form::Close();
+	}
+	};
+}
--- Project_Gul/MyForm.h
+++ Project_Gul/MyForm.h
@@ -1,5 +1,5 @@
 #pragma once
-
+#include "About.h"
 namespace ProjectGul {
 
 	using namespace System;
@@ -34,7 +34,19 @@ namespace ProjectGul {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ button2;
+
+	protected:
+
+	protected:
+
+
+
+
+
+
+
 	protected:
 
 	private:
@@ -50,40 +62,74 @@ namespace ProjectGul {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
-			// label1
+			// button1
 			// 
-			this->label1->AutoSize = true;
-			this->label1->Font = (gcnew System::Drawing::Font(L"Tahoma", 20));
-			this->label1->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(0)), static_cast<System::Int32>(static_cast<System::Byte>(0)),
-				static_cast<System::Int32>(static_cast<System::Byte>(0)), static_cast<System::Int32>(static_cast<System::Byte>(0)));
-			this->label1->Location = System::Drawing::Point(368, 172);
-			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(152, 33);
-			this->label1->TabIndex = 0;
-			this->label1->Text = L"Hello World";
-			this->label1->Click += gcnew System::EventHandler(this, &MyForm::label1_Click);
+			this->button1->BackColor = System::Drawing::Color::Red;
+			this->button1->CausesValidation = false;
+			this->button1->Cursor = System::Windows::Forms::Cursors::Hand;
+			this->button1->FlatAppearance->BorderSize = 0;
+			this->button1->Font = (gcnew System::Drawing::Font(L"Tahoma", 8.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button1->ForeColor = System::Drawing::Color::Black;
+			this->button1->Location = System::Drawing::Point(12, 12);
+			this->button1->Margin = System::Windows::Forms::Padding(0);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(68, 23);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"Exit";
+			this->button1->UseMnemonic = false;
+			this->button1->UseVisualStyleBackColor = false;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::Exit);
+			// 
+			// button2
+			// 
+			this->button2->BackColor = System::Drawing::SystemColors::ActiveCaptionText;
+			this->button2->Cursor = System::Windows::Forms::Cursors::Hand;
+			this->button2->Location = System::Drawing::Point(92, 12);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(75, 23);
+			this->button2->TabIndex = 1;
+			this->button2->Text = L"About";
+			this->button2->UseVisualStyleBackColor = false;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(892, 435);
-			this->Controls->Add(this->label1);
-			this->ForeColor = System::Drawing::Color::Crimson;
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->ForeColor = System::Drawing::Color::White;
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedSingle;
+			this->Margin = System::Windows::Forms::Padding(3, 4, 3, 4);
 			this->Name = L"MyForm";
 			this->Text = L"MyForm";
 			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
 			this->ResumeLayout(false);
-			this->PerformLayout();
 
 		}
 #pragma endregion
+	
 	private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
+		
 	}
-	private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
+	
+	
+	//Exit Button 
+	private: System::Void Exit(System::Object^ sender, System::EventArgs^ e) {
+		//show Message 
+		// if click yes it will Exit
+		if(MessageBox::Show("are you sure","Exit",MessageBoxButtons::YesNo,MessageBoxIcon::Question)==System::Windows::Forms::DialogResult::Yes)
+		   Application::Exit();
 	}
-	};
+	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+		About^ about = gcnew About;
+		about->ShowDialog();
+	}
+};
 }
